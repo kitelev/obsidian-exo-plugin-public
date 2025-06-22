bbe0e18e9d4682b0a977b88e60c0189af2babca3
kitelev <55425227+kitelev@users.noreply.github.com>
2025-06-22 09:37:50 +0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ export default class HelloNoticePlugin extends Plugin {
     this.addCommand({
       id: 'show-hello-notice',
       name: 'Show Hello Notice',
-      callback: () => new Notice('Hello!'),
+      callback: () => new Notice('Hello World!'),
     });
   }
 }
